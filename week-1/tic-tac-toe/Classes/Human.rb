fcef6ea34438479e0feb7960f638908decc6e4bb
yummta <yummta@gmail.com>
2019-03-04 09:29:49 -0500

    add tic-tac-toe project

---
+++ week-1/tic-tac-toe/Classes/Human.rb
@@ -0,0 +1,8 @@
+require "./Classes/Player.rb"
+
+class Human < Player
+	def move
+		puts "\nGive me your best move. You are X:"
+		return (gets.to_i - 1)
+	end
+end
---
+++ week-1/tic-tac-toe/launch_game.rb
@@ -0,0 +1,21 @@
+require "./Classes/Bot.rb"
+require "./Classes/Human.rb"
+require "./Classes/Game.rb"
+
+def create_game
+	player1 = Human.new("User", "x")
+	player2 = Bot.new("Bot", "o")
+	game = Game.new(player1, player2)
+	game
+end
+
+def init_game	
+	system "clear"
+	puts "1 2 3  |  - - -"
+	puts "4 5 6  |  - - -"
+	puts "7 8 9  |  - - -"
+	game = create_game
+	game.run_game
+end
+
+init_game
