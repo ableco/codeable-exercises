23396476eee2f817d8f5504f1fe020315d2d3f82
cristianbgp <cristian.granda.pastor@gmail.com>
2019-03-05 18:04:20 -0500

    rspec excercise in week2-day2

--- week-2/day-2/count.rb
+++ week-2/day-2/count.rb
@@ -1,21 +1,9 @@
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
 
-def count_positives_sum_negatives(array)
-  return [] if (array == nil || array == [])
-
-  positive_numbers = array.select do |v|
-    v.positive?
-  end.count
-  
-  negative_numbers = array.select do |v|
-    v.negative?
-  end.sum
-  
-  positive_and_negative = []
-  positive_and_negative << positive_numbers
-  positive_and_negative << negative_numbers
-  
-  return positive_and_negative
+def count_positives_sum_negatives(lst)
+  return [] if (lst == nil || lst == [])
+  group = lst.group_by {|val| val.positive?}
+  return [group[true].count, group[false].reduce(&:+)]
 end
 
 puts count_positives_sum_negatives(a)
