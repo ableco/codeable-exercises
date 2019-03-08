1d0829714a2e13624ae8451fa931b63e9b764fdf
Ricardo <ry.yrupailla@gmail.com>
2019-03-08 12:02:58 -0500

    fixed test , added main.rb

--- cli_app/cli_codeable.rb
+++ cli_app/cli_codeable.rb
@@ -73,29 +73,3 @@ def menu
   end
   menu
 end
-
-begin
-  while true == true
-  puts logo ="
-    ____          _            _     _      
-   / ___|___   __| | ___  __ _| |__ | | ___ 
-  | |   / _ \\ / _` |/ _ \\/ _` | '_ \\| |/ _ \\
-  | |__| (_) | (_| |  __/ (_| | |_) | |  __/
-   \\____\\___/ \\__,_|\\___|\\__,_|_.__/|_|\\___|                                  
-  ".blue
-  1.times {puts ""}
-
-  puts "Type: 't title of task' to add a new todo list item.".cyan
-  puts "Type: 't' to show your task list.".cyan
-  puts "Type: 't -d [task number]' to complete a task and clean.".cyan
-  puts "Type: 'quit' or 'q' to quit.".cyan
-
-  puts ""
-  show
-  menu
-  all
-  end
-
-rescue Interrupt => e
-  quit # forces the file to save even if the user [ctrl+c]s out
-end
