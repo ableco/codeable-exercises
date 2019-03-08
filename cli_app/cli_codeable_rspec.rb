9f61cde91ac1a367f0fd84c1ddaa34ae7871a398
Mayra Navarro <luciagirasoles@gmail.com>
2019-03-08 12:23:14 -0500

    se adiciono los casos de test como comentarios

--- cli_app/cli_codeable_rspec.rb
+++ cli_app/cli_codeable_rspec.rb
@@ -1,4 +1,12 @@
 require './cli_codeable.rb'
+# T1.Add a task:
+# Read more about El Niño
+# Write my first blog post!
+# T2 List the tasks
+# T3 Delete a task
+# T4 Delete a non existant ID: show an error
+# T5 Use an unexpected flag: show an error
+# T6 Add new tasks, ID should be the consecutive integer (skipping the ones deleted)
 
 RSpec.describe "test #cli_codeable" do
   
