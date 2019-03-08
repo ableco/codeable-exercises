769d20821f5d0878788c32a42cee51ace989deda
Ricardo <ry.yrupailla@gmail.com>
2019-03-08 14:04:19 -0500

    added delete test

--- cli_app/cli_codeable_rspec.rb
+++ cli_app/cli_codeable_rspec.rb
@@ -1,18 +1,27 @@
 require './cli_codeable.rb'
-# T1.Add a task: (Deyvi)
+# T1.Add a task:
 # - Read more about El Niño
 # - Write my first blog post!
-# T2 List the tasks (Mayra)
-# T3 Delete a task (Ricardo)
-# T4 Delete a non existant ID: show an error (Diego)
-# T5 Use an unexpected flag: show an error (Workteam)
+# T2 List the tasks
+# T3 Delete a task
+# T4 Delete a non existant ID: show an error
+# T5 Use an unexpected flag: show an error
 # T6 Add new tasks, ID should be the consecutive integer (skipping the ones deleted)
 
 RSpec.describe "test #cli_codeable" do
   
-  it "expect clear list of tasks" do
+  it "expect clear list of tasks" do    
     expect(clear_all()).to be_empty
   end
 
-end
+  it "expect delete one task" do
+    
+    add("t some task")
+    number_tasks_before = $task_array.length
+    delete('t -d 1')
+    number_tasks_after = $task_array.length   
+    task_number_change = number_tasks_before - number_tasks_after
+    expect(task_number_change).to eq(1)
+  end
 
+end
