b0bdc878ebbf677a13d1af2bbb01a6aa53ec6e8c
Carlos A <cayala.w@gmail.com>
2019-03-11 09:30:20 -0500

    Extended Project - Week 3: Web-scrapper

--- week-2/extended-project/yoda-y-sus-apis/web_api_methods.rb
+++ week-2/extended-project/yoda-y-sus-apis/web_api_methods.rb
@@ -2,6 +2,22 @@ require "http"
 require "json"
 require "csv"
 
+$index_general = 0
+$uri = "https://www.cnet.com/es/noticias/ranking-peliculas-star-wars-guerra-de-las-galaxias/"
+$response = ""
+
+def initialization_scrapping
+  $response = HTTP.get($uri).body.to_s
+  text_references = "Ránking de las películas de 'Star Wars' según CNET en Español"
+  $index_general = /#{text_references}/ =~ response
+end
+
+def get_score(name_movie)
+  index_movie = $response.index(name_movie, $index_general)
+  index_rating = $response.index(">:", index_movie) + 3
+  response[index_rating..(index_rating + 1)]
+end
+
 def get_data(page)
     data_next = []
     request = HTTP.get(page)
@@ -13,8 +29,9 @@ def get_data(page)
     return data + data_next
 end
 
-def create_file(input, filename)
+def create_file(input, headers, filename)
   CSV.open(filename + ".csv",'wb') do |data|
-    data << input.values
+    data << headers
+    input.each{ |val| data << val.values}
   end
 end
