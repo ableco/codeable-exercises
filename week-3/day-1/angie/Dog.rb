0dec4ec6c7723d6d9c866118c9b11297fe7d6422
Angie Cristina <angiecristinagonzalessara@gmail.com>
2019-03-11 19:03:47 -0500

    avance

---
+++ week-3/day-1/angie/Dog.rb
@@ -0,0 +1,13 @@
+with open('file') as f:
+    content = file.read()
+array = content.split()
+letters = map(int, array)
+print array
+
+def palindrome?(string)
+  if string == string.reverse
+    return true
+  else
+    puts “String is not a palindrome”
+  end
+ end
