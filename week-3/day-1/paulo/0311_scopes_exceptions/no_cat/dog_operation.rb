b2c81da92448e07173ab7a9d23d78a415b05cf2f
paulotijero <paulotijero@gmail.com>
2019-03-12 16:01:30 -0500

    Add dog_operation.rb test

--- week-3/day-1/paulo/0311_scopes_exceptions/no_cat/dog_operation.rb
+++ week-3/day-1/paulo/0311_scopes_exceptions/no_cat/dog_operation.rb
@@ -1,12 +1,29 @@
 require_relative "dog_class"
 
 def operation(user_input, dog)
-    if (user_input.scan(/^cat\s/).any?)
-        op = user_input.split("\s")
-        if (op[1].scan(/[>]/).any?)
-            return "add file"
-        end
+    
+    # Devuelve true con el siguiente formato "cat > file.txt"
+    if (user_input.scan(/^cat\s>\s\w+\.txt$/).any?)
+      return "create file"
+  
+    # Devuelve true con el siguiente formato "cat >> file.txt"
+    elsif (user_input.scan(/^cat\s>>\s\w+\.txt$/).any?)
+      return "add file"
+    
+    # Devuelve true con el siguiente formato "cat file.txt"
+    elsif (user_input.scan(/^cat\s\w+\.txt$/).any?)
+      return "view file"
+
+    # Devuelve true con el siguiente formato "cat file1.txt file2.txt"
+    elsif (user_input.scan(/^cat\s\w+\.txt\s\w+\.txt$/).any?)
+      return "concat file"
+
+    # Devuelve true con el siguiente formato "cat file1.txt file2.txt > file3.txt"
+    elsif (user_input.scan(/^cat\s\w+\.txt\s\w+\.txt\s>\s\w+\.txt$/).any?)
+      return "concat and transfer file"
+
     else
-        return "Error: Unexpected input"
+      return "Error: Unexpected input"
     end
+    
 end
--- week-3/day-1/paulo/0311_scopes_exceptions/no_cat/dog_rspec.rb
+++ week-3/day-1/paulo/0311_scopes_exceptions/no_cat/dog_rspec.rb
@@ -15,10 +15,25 @@ RSpec.describe "Test to dog" do
             expect(operation("ererer", dog)).to eq("Error: Unexpected input")
         end
 
-        it "Add file" do
-            expect(operation("cat > hola.txt", dog)).to eq("add file")
+        it "Create file" do
+            expect(operation("cat > hola.txt", dog)).to eq("create file")
         end
 
+        it "Add file" do
+            expect(operation("cat >> hola.txt", dog)).to eq("add file")
+        end
+
+        it "View file" do
+            expect(operation("cat hola.txt", dog)).to eq("view file")
+        end
+
+        it "Concat file" do
+            expect(operation("cat hola1.txt hola2.txt", dog)).to eq("concat file")
+        end
+
+        it "Concat and transfer file" do
+            expect(operation("cat hola1.txt hola2.txt > hola3.txt", dog)).to eq("concat and transfer file")
+        end
     end
 
 end
