efbd2001ccc14edd4dfdf069f12b15a405078892
paulotijero <paulotijero@gmail.com>
2019-03-12 16:18:21 -0500

    Clean blank files

--- week-3/day-1/paulo/0311_scopes_exceptions/material/exceptions.rb
+++
@@ -1,1 +0,0 @@
-# Create a script that receives the path to a .json file with an array and any string, read the file, parse it as JSON, append the string at the top of the file and write it again to the disk.
