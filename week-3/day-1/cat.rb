182a8d0bcf10e598bab46bfa2ba390fa895dd627
Mayra Navarro <luciagirasoles@gmail.com>
2019-03-11 22:48:34 -0500

    Adding task explanation

--- week-3/day-1/cat.rb
+++ week-3/day-1/cat.rb
@@ -1,3 +1,5 @@
+#Create a copy of the cat command using Ruby and call it dog. 
+#Be sure to support reading multiple files and copying the content to another file.
 class Archivo
     attr_accessor :name
     def initialize(input)
