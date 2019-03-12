8f1affbab4e791c3a22074f7175901ebd19d30b6
paulotijero <paulotijero@gmail.com>
2019-03-11 23:21:56 -0500

    Fix return operation

--- week-3/day-1/paulo/0311_scopes_exceptions/no_cat/dog_operation.rb
+++ week-3/day-1/paulo/0311_scopes_exceptions/no_cat/dog_operation.rb
@@ -2,6 +2,7 @@ require_relative "dog_class"
 
 def operation(user_input, dog)
     if (user_input.scan(/^cat\s/).any?)
+        return "Hi"
     else
         return "Error: Unexpected input"
     end
