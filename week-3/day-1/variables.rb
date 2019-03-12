7965098448fb5d92599b878b3dc11c144082d886
Marieth <mperezzevallos@gmail.com>
2019-03-11 20:55:13 -0500

    Polindromes

---
+++ week-3/day-1/variables.rb
@@ -0,0 +1,20 @@
+$global="mundo"
+
+class Test
+
+    @@tipo_trabajador="Administrativos"
+
+    def initialize(name)
+        @name=name
+        
+    end
+
+    def mostrar
+
+        
+
+    end
+
+
+
+end
