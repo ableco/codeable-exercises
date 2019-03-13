226c520261eeb886ea735c3393fe8456a50a1ab7
Marieth <mperezzevallos@gmail.com>
2019-03-12 21:52:41 -0500

    Caesar

---
+++ week-3/day-2/Caesar.rb
@@ -0,0 +1,56 @@
+#Write a class that, when given a string, will return an uppercase string with each letter shifted forward in the alphabet by however many spots the cipher was initialized to.
+class CaesarCipher
+    def initialize(shift)
+      #your code here
+      @shift=shift
+    end
+  
+    def encode(string)
+      #your code here
+      string=string.upcase
+      string_encode=string.chars.map do |caracter|
+        if !!(/[A-Z]/ =~ caracter)
+            if caracter.ord+@shift >90
+                (caracter.ord+@shift-26).chr
+            else
+                (caracter.ord+@shift).chr
+            end 
+        else
+            caracter
+        end
+      end
+      string_encode.join
+    end
+    
+    def decode(string)
+      #your code here
+      string=string.upcase
+      string_encode=string.chars.map do |caracter|
+        if !!(/[A-Z]/ =~ caracter)
+            if caracter.ord-@shift <65
+                (caracter.ord-@shift+26).chr
+            else
+                (caracter.ord-@shift).chr
+            end 
+        else
+            caracter
+        end    
+      end
+      string_encode.join
+    end
+  end
+
+
+  c=CaesarCipher.new(5)
+
+  puts c.encode("IT'S A SHIFT CIPHER!!").inspect
+  puts c.decode("NY'X F XMNKY HNUMJW!!").inspect
+  
+
+# array ="CODE".chars.map { |i|  i}
+
+# puts array.inspect
+
+
+
+
