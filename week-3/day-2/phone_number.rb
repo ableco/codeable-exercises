3b9144b760dfd436806350f6892eda46952e8cb6
Ricardo <ry.yrupailla@gmail.com>
2019-03-13 17:30:16 -0500

    deleted programs

--- week-3/day-2/phone_number.rb
+++
@@ -1,8 +0,0 @@
-def createPhoneNumber(numbers)
-  code = numbers[(0..2)].join("")
-  section_1 = numbers[(3..5)].join("")
-  section_2 = numbers[(6..9)].join("")  
-  return "(#{code}) #{section_1}-#{section_2}"
-end
-
-puts createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
