3d65b80dbe918cb0696b6264ad0c024e06c75b62
cristianbgp <cristian.granda.pastor@gmail.com>
2019-03-14 14:01:06 -0500

    Finished

--- week-3/day-3/mailbox_erb-2.rb
+++ week-3/day-3/mailbox_erb-2.rb
@@ -1,50 +1,32 @@
 require "erb"
 
 class Email
-  def initialize(subject, headers)
+  attr_accessor :subject, :date, :from
+  def initialize(subject, date:, from:)
     @subject = subject
-    @headers = headers
-  end
-
-  def subject
-    @subject
-  end
-
-  def date
-    @headers[:date]
-  end
-
-  def from
-    @headers[:from]
+    @date = date
+    @from = from
   end
 end
 
 class Mailbox
+  attr_accessor :name, :emails
   def initialize(name, emails)
     @name = name
     @emails = emails
   end
-
-  def name
-    @name
-  end
-
-  def emails
-    @emails
-  end
 end
 
 class MailboxErbRenderer
+  attr_accessor :mailbox, :filename
   def initialize(mailbox, filename)
     @mailbox = mailbox
     @filename = filename
   end
 
   def render
-    # imaginary save file
-    File.open(@filename, "w") do |file|
-      file.write(@mailbox)
-    end
+    template = File.read(@filename)
+    ERB.new(template).result(binding)
   end
 end
 
@@ -55,46 +37,16 @@ emails = [
 ]
 
 
-
-template = "
-<html>
-  <head>
-    <style>
-      table {
-        border-collapse: collapse;
-      }
-      td, th {
-        border: 1px solid black;
-        padding: 1em;
-      }
-    </style>
-  </head>
-  <body>
-    <h1><%= mailbox.name %></h1>
-    <table>
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>From</th>
-          <th>Subject</th>
-        </tr>
-      </thead>
-      <tbody>
-        <% mailbox.emails.each do |email| %>
-        <tr>
-          <td><%= email.date %></td>
-          <td><%= email.from %></td>
-          <td><%= email.subject %></td>
-        </tr>
-        <% end %>
-      </tbody>
-    </table>
-  </body>
-</html>
-"
 mailbox = Mailbox.new("Ruby Study Group", emails)
-
 renderer = MailboxErbRenderer.new(mailbox, "mailbox.erb")
-html = renderer.render
+$html = renderer.render
 
-puts html
+def save_html
+  File.open("mailbox.html","w") do |file|
+    file.write($html)
+  end
+end
+
+save_html
+
+puts $html
