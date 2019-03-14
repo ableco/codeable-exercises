762fe4e2b3b17a3a4483632b55c89ce1458bd383
paul <yummta@gmail.com>
2019-03-13 22:29:14 -0500

    mini day 3

---
+++ week-3/day-3/paul/generator_html.rb
@@ -0,0 +1,68 @@
+require "erb"
+
+template="
+<html>
+<head>
+  <style>
+    table {
+      border-collapse: collapse;
+    }
+    td, th {
+      border: 1px solid black;
+      padding: 1em;
+    }
+  </style>
+</head>
+<body>
+  <h1><%= mailbox.title%></h1>
+  <table>
+    <thead>
+      <tr>
+        <th>Date</th>
+        <th>From</th>
+        <th>Subject</th>
+      </tr>
+    </thead>
+    <tbody>
+      <% mailbox.emails.each do |email| %>
+      <tr>
+        <td><%= email.date %></td>
+        <td><%= email.from %></td>
+        <td><%= email.subject %></td>
+      </tr>
+      <% end %>
+    </tbody>
+  </table>
+</body>
+</html>
+  "
+
+class Email
+  attr_accessor :subject, :date, :from
+
+  def initialize(subject, date:, from:)
+    @subject = subject
+    @date = date
+    @from = from
+  end
+end
+
+
+class Mailbox
+  attr_accessor :title, :emails
+
+  def initialize(title, emails)
+    @title = title
+    @emails = emails
+  end
+
+end
+emails = [
+  Email.new("Homework this week", date: "2014-12-01", from: "Ferdous"),
+  Email.new("Keep on coding! :)", date: "2014-12-01", from: "Dajana"),
+  Email.new("Re: Homework this week", date: "2014-12-02", from: "Ariane")
+]
+mailbox = Mailbox.new("Ruby Study Group", emails)
+
+html = ERB.new(template).result(binding)
+puts html
