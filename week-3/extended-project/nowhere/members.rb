7f3277e7ad22246e2999e714519e0fedf1bdcdd7
cristianbgp <cristian.granda.pastor@gmail.com>
2019-03-15 16:19:46 -0500

    view members

--- week-3/extended-project/nowhere/members.rb
+++ week-3/extended-project/nowhere/members.rb
@@ -6,9 +6,10 @@
 # What's the best way to convince you to do something?
 # How do you like to get feedback?
 
-team = {
+TEAM = {
   :lian => {
     :name => "Lian Richelle Nivin Suarez",
+    :image => "https://camo.githubusercontent.com/f1939212b2109226afcce2c4a83ddcb6655f71aa/68747470733a2f2f64726976652e676f6f676c652e636f6d2f75633f6578706f72743d766965772669643d314452377a6c6b573656577776734b6d313145443964305f6657446c625879744f",
     :honest => "I try whenever I can speak directly and honestly, even if that can make someone uncomfortable",
     :nuts => "If someone is very disrespectful, that makes me crazy",
     :gold_star => "I always maintain relationships with people that I feel are very sincere",
@@ -18,6 +19,7 @@ team = {
     },
   :deyvi => {
     :name => "Deyvi",
+    :image => "https://scontent.flim19-1.fna.fbcdn.net/v/t1.0-9/53524149_1176409392536075_6453008463239839744_n.jpg?_nc_cat=107&_nc_ht=scontent.flim19-1.fna&oh=480eaab0d5905680efe667e976f24962&oe=5D26574E",
     :honest => "I'm passionate and a risk taker",
     :nuts => "Dismotivated people",
     :gold_star => "I have amazing memes in my pocket",
@@ -27,6 +29,7 @@ team = {
     },
   :frank => {
     :name => "Frank Condezo",
+    :image => "https://camo.githubusercontent.com/0f254c6682d9b7eb7b29b580ae64dd91b99a8a47/68747470733a2f2f64726976652e676f6f676c652e636f6d2f75633f6578706f72743d766965772669643d314754353635346f645f4a38437a54415757646b334d6d5a7a726e4d6b67684a48",
     :honest => "I like anime",
     :nuts => "That they do not take responsibility for their actions",
     :gold_star => "They have to be authentic/genuine",
@@ -36,6 +39,7 @@ team = {
     },
   :cristian => {
     :name => "Cristian",
+    :image => "https://camo.githubusercontent.com/972db7dfa697365dbc42f6c91f644a6d6d8a37c8/68747470733a2f2f64726976652e676f6f676c652e636f6d2f75633f6578706f72743d766965772669643d315779374f51696831686e30587747585f696a68624f634354415562594965524c",
     :honest => "I'm good listener",
     :nuts => "Excessive noise or absolute silence",
     :gold_star => "If you like games",
