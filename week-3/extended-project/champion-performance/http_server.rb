ab60f134a99f8709762f49091bc39a24d2d7fc75
Ricardo <ry.yrupailla@gmail.com>
2019-03-15 10:28:16 -0500

    add server files

---
+++ week-3/extended-project/champion-performance/http_server.rb
@@ -0,0 +1,112 @@
+require 'socket'
+require 'uri'
+
+WEB_ROOT = './public'
+
+@html_test = "
+<html>
+  <body>
+    <div style=text-align: center>
+      <h1>Hello World!</h1>
+      <img src=earth_heart.jpg />
+    </div>
+  </body>
+</html>
+"
+
+
+def requested_file(request_line, socket)  
+
+  request_uri  = request_line.split(" ")[1]
+  STDERR.puts "petition to : #{request_uri}" 
+
+  case request_uri
+  when /^(\/)$/
+
+    STDERR.puts "first option"     
+    response_from_path(WEB_ROOT, socket)
+
+  when /^(\/\w+\.\w+)$/
+
+    STDERR.puts "second option" 
+    path   = URI.unescape(URI(request_uri).path)  
+
+    clean = []
+    parts = path.split("/")
+  
+    parts.each do |part|
+      next if part.empty? || part == '.'
+      part == '..' ? clean.pop : clean << part
+    end  
+  
+    path = File.join(WEB_ROOT, *clean)
+    response_from_path(path, socket)
+
+  when /^(\/\?name=\w+)$/
+    STDERR.puts "third option"
+
+    paramstring = request_uri.split('?')[1]
+    name = paramstring.split('=')[1]
+
+    response_from_string(@html_test,socket)
+
+  else
+      print "something else"
+  end  
+  
+end
+
+def response_from_string(file, socket)
+
+  socket.print "HTTP/1.1 200 OK\r\n" +
+  "Content-Type: text/html\r\n" +
+  "Content-Length: #{file.size}\r\n" +
+  "Connection: close\r\n"
+
+  socket.print "\r\n"
+  socket.print file
+
+  #IO.copy_stream(file, socket)
+end
+
+
+def response_from_path(path, socket)
+  path = File.join(path, 'index.html') if File.directory?(path)
+
+  if File.exist?(path) && !File.directory?(path)
+    File.open(path, "rb") do |file|
+      socket.print "HTTP/1.1 200 OK\r\n" +
+                   "Content-Type: text/html\r\n" +
+                   "Content-Length: #{file.size}\r\n" +
+                   "Connection: close\r\n"
+
+      socket.print "\r\n"
+
+      IO.copy_stream(file, socket)
+    end
+  else
+    message = "File not found\n"
+
+    socket.print "HTTP/1.1 404 Not Found\r\n" +
+                 "Content-Type: text/plain\r\n" +
+                 "Content-Length: #{message.size}\r\n" +
+                 "Connection: close\r\n"
+
+    socket.print "\r\n"
+
+    socket.print message
+  end
+end
+
+server = TCPServer.new('localhost', 2345)
+
+loop do
+  socket = server.accept
+  request_line = socket.gets
+
+  STDERR.puts request_line
+
+  requested_file(request_line, socket)
+
+  socket.close
+end
