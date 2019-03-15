9330523c66e705c98a4de66b519c4903b63d8c89
paul <yummta@gmail.com>
2019-03-15 10:36:01 -0500

    integrate erb with json

--- week-3/extended-project/champion-performance/controller/controller.rb
+++ week-3/extended-project/champion-performance/controller/controller.rb
@@ -1,14 +1,26 @@
+require "json"
 require "erb"
-require "./model/team_members.rb"
 
-def controller
+title = "patricia"
 
-  title = "PATRICIA"
-  template = File.read("./team_member.erb")
+def controller(person)
+  # get json data
+  data = File.open("./model/team_members.json")
+  data_team_members = ""
+  data.each { |x| data_team_members << x }
+  data.close
+  data_team_members = JSON.parse(data_team_members)
 
+  # creating accesible variables
+  name = data_team_members[person]["name"]
+  team = data_team_members[person]["team"]
+  introduction = data_team_members[person]["introduction"]
+
+  # get file template
+  template = File.read("./templates/team_member.erb")
+
+  # get html
   html = ERB.new(template).result(binding)
-
-  puts html
 end
 
-controller
+controller("paul")
