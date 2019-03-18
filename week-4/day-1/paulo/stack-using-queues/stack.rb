ef1a9957a8fbbdd90c856bcf7252b28382beb0ea
paulotijero <paulotijero@gmail.com>
2019-03-18 18:01:35 -0500

    Add solution to Implement stack using queues

---
+++ week-4/day-1/paulo/stack-using-queues/stack.rb
@@ -0,0 +1,23 @@
+class MyStack
+
+  def initialize()
+      @data = []
+  end
+
+  def push(x)
+      @data.push(x)
+  end
+
+  def pop()
+      @data.pop()
+  end
+
+  def top()
+      @data.last
+  end
+
+  def empty()
+      @data.empty?
+  end
+
+end
