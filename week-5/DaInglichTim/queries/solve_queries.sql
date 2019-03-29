2266e1881625b90dc821a21ddbd405760f3793b1
paulotijero <paulotijero@gmail.com>
2019-03-29 18:38:42 -0500

    Add solve_queries pt.2

--- week-5/DaInglichTim/queries/solve_queries.sql
+++ week-5/DaInglichTim/queries/solve_queries.sql
@@ -24,3 +24,37 @@ SELECT payment_type, COUNT(*) as counter
 FROM transactions
 GROUP BY payment_type
 ORDER BY counter DESC LIMIT 1;
+
+-- Per each tour, where do customers come from? Show the top 5 locations
+-- Inka trail
+
+SELECT c.customer_city, COUNT(*) "cantidad"
+FROM products p
+  INNER JOIN transactions t on t.id_product = p.id_product
+  INNER JOIN customers c on c.id_customer = t.id_customer
+WHERE p.product = 'Inka trail' 
+GROUP BY c.customer_city 
+ORDER BY cantidad DESC
+LIMIT 5;
+
+-- Caribbean Tour
+
+SELECT c.customer_city, COUNT(*) "cantidad"
+FROM products p
+  INNER JOIN transactions t on t.id_product = p.id_product
+  INNER JOIN customers c on c.id_customer = t.id_customer
+WHERE p.product = 'Caribbean Tour' 
+GROUP BY c.customer_city 
+ORDER BY cantidad DESC
+LIMIT 5;
+
+-- Romantic Paris
+
+SELECT p.product ,c.customer_city, COUNT(*) "cantidad"
+FROM products p
+  INNER JOIN transactions t on t.id_product = p.id_product
+  INNER JOIN customers c on c.id_customer = t.id_customer
+WHERE p.product = 'Romantic Paris' 
+GROUP BY c.customer_city 
+ORDER BY cantidad DESC
+LIMIT 5;
