9e8fa2db5440c4688517a5e3ea193a9a9ef60ef3
paulotijero <paulotijero@gmail.com>
2019-03-29 15:45:44 -0500

    Add insert generate

---
+++ week-5/DaInglichTim/queries/product.sql
@@ -0,0 +1,3 @@
+INSERT INTO products values (default,'Caribbean Tour','9','1200');
+INSERT INTO products values (default,'Romantic Paris','3','3600');
+INSERT INTO products values (default,'Inka trail','7','7500');
