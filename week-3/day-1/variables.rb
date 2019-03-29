2d92719f013053bca6dac87f828db8c2da50272f
Kattya Cuevas <kattya.cuevas@gmail.com>
2019-03-29 15:49:20 -0500

    Revert "Exercise 1"
    This reverts commit 821c41b4bcd2e37c9242d492ed8d8b39c44f9ff8.

--- week-3/day-1/variables.rb
+++ week-3/day-1/variables.rb
@@ -1,4 +1,4 @@
-$global="Mundo"
+$global="mundo"
 
 class Test
 
@@ -10,27 +10,11 @@ class Test
     end
 
     def mostrar
-        var_local="Operaciones"
-        variables
-        $global="Nuevo Mundo "
-        @@tipo_trabajador="Nuevos Administrativos"
-        @name="Nuevo nombre"
-        variables
+
+        
 
     end
 
-    def variables
-        puts $global
-        puts @@tipo_trabajador
-        puts @name
-    
-    end
+
 
 end
-
-
-test_a= Test.new("MARIA")
-test_a.mostrar
-
-test_b= Test.new("JUAN")
-test_b.mostrar
